85ec9b19e5f10ac27c026d74e6c7596671f47fb2
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-11-13 10:28:37 -0300

    feat: Movendo injeções de dependencias não utilizadas depois da separação de classe

--- src/main/java/com/algafood/algafoodapi/service/cliente/ClienteService.java
+++ src/main/java/com/algafood/algafoodapi/service/cliente/ClienteService.java
@@ -1,8 +1,5 @@
 package com.algafood.algafoodapi.service.cliente;
 
-import com.algafood.algafoodapi.anotations.TipoNotificador;
-import com.algafood.algafoodapi.enums.NivelUrgencia;
-import com.algafood.algafoodapi.interfaces.Notificador;
 import com.algafood.algafoodapi.model.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
@@ -11,10 +8,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClienteService {
-
-    @TipoNotificador(NivelUrgencia.URGENTE)
-    @Autowired
-    private Notificador notificador;
 
     @Autowired
     private ApplicationEventPublisher publisher;
