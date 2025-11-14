75c86beec63b41e746f6e39b8f5636ed8e32ee5f
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-11-14 17:44:00 -0300

    feat: Criando um serviço REST de Cozinha

---
+++ src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafoodapi.api.controller;
+
+import com.algaworks.algafoodapi.domain.entity.Cozinha;
+import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("/listar")
+    public List<Cozinha> listar() {
+        return cozinhaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscarPorId(@PathVariable Long id) {
+        return cozinhaRepository.findById(id);
+    }
+
+    @PostMapping
+    public Cozinha inserirOrUpdate(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.insertOrUpdate(cozinha);
+    }
+}
