fa4c59d2dd237363cdf3d6f8665e8a4bea097812
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-11-17 15:31:21 -0300

    feat:ajustando a respostas HTTP em cozinha por ID

--- src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -4,6 +4,9 @@ import com.algaworks.algafoodapi.api.model.CozinhasXmlRepresentation;
 import com.algaworks.algafoodapi.domain.entity.Cozinha;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +18,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping(value = "/listar", produces = "application/xml")
+    @GetMapping(value = "/listar", produces = MediaType.APPLICATION_XML_VALUE)
     public CozinhasXmlRepresentation listarEmXml() {
         return new CozinhasXmlRepresentation(cozinhaRepository.findAll());
     }
@@ -26,8 +29,14 @@ public class CozinhaController {
     }
 
     @GetMapping("/{id}")
-    public Cozinha buscarPorId(@PathVariable Long id) {
-        return cozinhaRepository.findById(id);
+    public ResponseEntity<Cozinha> buscarPorId(@PathVariable Long id) {
+        Cozinha cozinha = cozinhaRepository.findById(id);
+
+        if(cozinha == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(cozinha);
     }
 
     @PostMapping
