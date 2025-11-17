1064d7a66efb5c088ac0ae9d6d4e8684942a7905
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-11-17 14:24:26 -0300

    feat: Mapeando o objeto de cozinha para ele retornar a lista de cozinhas em XML e customizando o nome dos atributos no response do XML

--- src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafoodapi.api.controller;
 
+import com.algaworks.algafoodapi.api.model.CozinhasXmlRepresentation;
 import com.algaworks.algafoodapi.domain.entity.Cozinha;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,11 @@ public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
+
+    @GetMapping(value = "/listar", produces = "application/xml")
+    public CozinhasXmlRepresentation listarEmXml() {
+        return new CozinhasXmlRepresentation(cozinhaRepository.findAll());
+    }
 
     @GetMapping("/listar")
     public List<Cozinha> listar() {
