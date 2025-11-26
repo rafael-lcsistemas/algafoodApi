21215a2353fc25979d735c5fe9fc636f8b7521a0
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-11-26 08:44:37 -0300

    feat: Craindo entidade GRUPO
    feta: Adicionamos o relacionamento muitos para muitos @ ManyToMany de GRUPO com permissao
    
    feat: Adicionamos os inserts de Grupo, Permissão e Grupo_Permissao

---
+++ src/main/java/com/algaworks/algafoodapi/domain/model/entity/Grupo.java
@@ -0,0 +1,73 @@
+package com.algaworks.algafoodapi.domain.model.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Grupo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+            joinColumns = @JoinColumn(name = "id_grupo"),
+            inverseJoinColumns = @JoinColumn(name = "id_permissao"))
+    private List<Permissao> permissoes = new ArrayList<>();
+
+    public Grupo() {
+    }
+
+    public Grupo(Long id, String descricao, List<Permissao> permissoes) {
+        this.id = id;
+        this.descricao = descricao;
+        this.permissoes = permissoes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Permissao> getPermissoes() {
+        return permissoes;
+    }
+
+    public void setPermissoes(List<Permissao> permissoes) {
+        this.permissoes = permissoes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Grupo grupo = (Grupo) o;
+        return Objects.equals(id, grupo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Grupo: " + id + " / " + descricao + " / Permissões: " + permissoes;
+    }
+}
