dac43e49a84eb6d235666244e634be44ef06e85a
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-12-03 15:24:19 -0300

    feat: Removendo exceção não utilizada

--- src/main/java/com/algaworks/algafoodapi/domain/exceptions/EntidadeIntegridadeException.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafoodapi.domain.exceptions;
-
-public class EntidadeIntegridadeException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-
-    public EntidadeIntegridadeException(String mensagem) {
-        super(mensagem);
-    }
-}
