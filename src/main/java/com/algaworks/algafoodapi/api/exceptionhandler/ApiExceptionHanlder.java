20aba4e5200519a75c32274c4ab40e012a482424
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-12-03 15:44:56 -0300

    feat: Usando extensão da Exceções padrão do Spring para aproveitar a mãe de obra de ter que programar 1 a 1

--- src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHanlder.java
+++ src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHanlder.java
@@ -3,16 +3,22 @@ package com.algaworks.algafoodapi.api.exceptionhandler;
 import com.algaworks.algafoodapi.domain.exceptions.EntidadeEmUsoException;
 import com.algaworks.algafoodapi.domain.exceptions.EntidadeNaoEncontradaException;
 import com.algaworks.algafoodapi.domain.exceptions.NegocioException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @ControllerAdvice
-public class ApiExceptionHanlder {
+public class ApiExceptionHanlder extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
@@ -32,15 +38,6 @@ public class ApiExceptionHanlder {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-        Problema problema = new Problema();
-        problema.setDataHora(LocalDateTime.now());
-        problema.setMensagem("Tipo do arquivo de entrada não suportado.");
-
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
-    }
-
     @ExceptionHandler(EntidadeEmUsoException.class)
     public ResponseEntity<?> tratarEntidadeEmUso(EntidadeEmUsoException e) {
 
@@ -49,5 +46,21 @@ public class ApiExceptionHanlder {
         problema.setMensagem(e.getMessage());
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<MediaType> mediaTypes = ex.getSupportedMediaTypes();
+        if (!CollectionUtils.isEmpty(mediaTypes)) {
+            headers.setAccept(mediaTypes);
+        }
+
+        Problema problema = new Problema();
+        problema.setDataHora(LocalDateTime.now());
+        problema.setMensagem("Tipo de entrada do arquivo não suportado.");
+
+        return handleExceptionInternal(ex, problema, headers, status, request);
     }
 }
