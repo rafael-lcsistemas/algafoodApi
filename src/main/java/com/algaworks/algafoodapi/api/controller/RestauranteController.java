d62aceb7a12e3cdcac43cfbecccb4241f8065d6b
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-12-10 17:05:25 -0300

    feat: Ajustando as classe RestauranteController para a classes generica de Assembler

--- src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafoodapi.api.controller;
 
-import com.algaworks.algafoodapi.api.assembler.RestauranteModelAssembler;
-import com.algaworks.algafoodapi.api.assembler.RestauranteModelDisassembler;
+import com.algaworks.algafoodapi.api.assembler.GenericInputAssembler;
+import com.algaworks.algafoodapi.api.assembler.GenericResponseAssembler;
 import com.algaworks.algafoodapi.api.model.input.RestauranteInput;
 import com.algaworks.algafoodapi.api.model.response.RestauranteResponse;
 import com.algaworks.algafoodapi.domain.exceptions.CozinhaNaoEncontradaException;
@@ -25,33 +25,36 @@ public class RestauranteController {
     private RestauranteService restauranteService;
 
     @Autowired
-    private RestauranteModelAssembler restauranteModelAssembler;
+    private GenericResponseAssembler genericResponseAssembler;
 
     @Autowired
-    private RestauranteModelDisassembler restauranteModelDisassembler;
+    private GenericInputAssembler genericInputAssembler;
 
     @GetMapping("/listar")
     public List<RestauranteResponse> filtrarTodas() {
-        return restauranteModelAssembler.toColletionModel(restauranteService.filtrarTodas());
+        return genericResponseAssembler.toCollectionModel(
+                restauranteService.filtrarTodas(), RestauranteResponse.class);
     }
 
     @GetMapping("/por-nome")
     public List<RestauranteResponse> filtrarNome(String nome) {
-        return restauranteModelAssembler.toColletionModel(restauranteService.filtrarPorNome(nome));
+        return genericResponseAssembler.toCollectionModel(
+                restauranteService.filtrarPorNome(nome), RestauranteResponse.class);
     }
 
     @GetMapping("{id}")
     public RestauranteResponse filtrarPorId(@PathVariable Long id) {
         Restaurante restaurante = restauranteService.filtrarPorID(id);
-        return restauranteModelAssembler.toModel(restaurante);
+        return genericResponseAssembler.toModel(restaurante, RestauranteResponse.class);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public RestauranteResponse inserir(@RequestBody @Valid RestauranteInput restauranteInput) {
         try {
-            Restaurante restaurante = restauranteModelDisassembler.toEntity(restauranteInput);
-            return restauranteModelAssembler.toModel(restauranteService.inserirOuAtualizar(restaurante));
+            Restaurante restaurante = genericInputAssembler.toEntity(restauranteInput, Restaurante.class);
+            return genericResponseAssembler.toModel(
+                    restauranteService.inserirOuAtualizar(restaurante), RestauranteResponse.class);
         } catch (CozinhaNaoEncontradaException | FormaPagamentoNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
         }
@@ -59,12 +62,13 @@ public class RestauranteController {
 
     @PutMapping("/{id}")
     public RestauranteResponse atualizar(@PathVariable Long id, @RequestBody @Valid RestauranteInput restauranteInput) {
-        var restaurante = restauranteModelDisassembler.toEntity(restauranteInput);
+        var restaurante = genericInputAssembler.toEntity(restauranteInput, Restaurante.class);
         var restauranteAtual = restauranteService.filtrarPorID(id);
         BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "endereco", "dataCadastro");
 
         try {
-            return restauranteModelAssembler.toModel(restauranteService.inserirOuAtualizar(restauranteAtual));
+            return genericResponseAssembler.toModel(
+                    restauranteService.inserirOuAtualizar(restauranteAtual), RestauranteResponse.class);
         } catch (CozinhaNaoEncontradaException | FormaPagamentoNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
         }
