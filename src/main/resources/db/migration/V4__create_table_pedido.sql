f44d5166100de21ec05de8e24412cb2caacb8132
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-12-11 15:13:34 -0300

    feat: Organizando a criação da table de PedidoDet

--- src/main/resources/db/migration/V4__create_table_pedido.sql
+++ src/main/resources/db/migration/V4__create_table_pedido.sql
@@ -7,7 +7,7 @@ CREATE TABLE pedido
     subtotal              DECIMAL               NOT NULL,
     id_usuario            BIGINT                NOT NULL,
     id_restaurante        BIGINT                NOT NULL,
-    id_forma_pagamento     BIGINT                NOT NULL,
+    id_forma_pagamento    BIGINT                NOT NULL,
     status_pedido         VARCHAR(10)           NOT NULL,
     datahora_pedido       datetime              NOT NULL,
     datahora_confirmacao  datetime              NULL,
@@ -26,7 +26,7 @@ CREATE TABLE pedido
 CREATE TABLE pedidodet
 (
     id             BIGINT AUTO_INCREMENT NOT NULL,
-    id_pedido      BIGINT                NULL,
+    id_pedido      BIGINT                NOT NULL,
     id_produto     BIGINT                NOT NULL,
     preco          DECIMAL               NOT NULL,
     quantidade     DECIMAL               NOT NULL,
@@ -49,9 +49,6 @@ ALTER TABLE pedido
 
 ALTER TABLE pedido
     ADD CONSTRAINT FK_PEDIDO_ON_ID_USUARIO FOREIGN KEY (id_usuario) REFERENCES usuario (id);
-
-ALTER TABLE pedidodet
-    ADD CONSTRAINT uc_pedidodet_id_produto UNIQUE (id_produto);
 
 ALTER TABLE pedidodet
     ADD CONSTRAINT FK_PEDIDODET_ON_ID_PEDIDO FOREIGN KEY (id_pedido) REFERENCES pedido (id);
