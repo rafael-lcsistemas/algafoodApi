9573cac0330eb0ca25874016b90295653834a093
Milton Rafael da Silva Nunes <rafael.nunes@lcsistemas.com.br>
2025-12-12 09:43:30 -0300

    feat: Configurando o ModelMapperConfig para setar os dados corretos de Cidade e Estado no DTO de endereço e ficar melhor visivelmente

--- src/main/java/com/algaworks/algafoodapi/core/mapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/mapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafoodapi.core.mapper;
 
+import com.algaworks.algafoodapi.api.model.response.EnderecoResponse;
+import com.algaworks.algafoodapi.domain.model.entity.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,17 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+
+        // EnderecoResponse -> Endereco
+        var enderecoResponseTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoResponse.class);
+
+        enderecoResponseTypeMap.<String>addMapping(src -> src.getCidade().getEstado().getNome(),
+                (dest, value) -> dest.getCidade().setEstado(value));
+
+        enderecoResponseTypeMap.<String>addMapping(src -> src.getCidade().getEstado().getUf(),
+                (dest, value) -> dest.getCidade().setUf(value));
+
+        return modelMapper;
     }
 }
